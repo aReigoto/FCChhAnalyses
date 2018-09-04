b6f27a18f677eaa16ce2908403da0215a064912d
aReigoto <andrereigoto@gmai.com>
2018-09-04 18:20:17 +0100

    a

--- HELHC/tth_new/TreeProducer.py
+++ HELHC/tth_new/TreeProducer.py
@@ -64,7 +64,7 @@ class SimpleTreeProducer(Analyzer):
          has processed the event.
 
         '''
-        self.tree.fill('weights', event.input.mcEventWeights)
+        self.tree.fill('weights', event.input.weights)
         self.tree.fill('electrons', event.input.electrons)
         self.tree.tree.Fill()
 
