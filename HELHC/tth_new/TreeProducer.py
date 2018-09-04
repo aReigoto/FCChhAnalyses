c49ea3533bf4675d31787071c3393ddb0e2c8d97
aReigoto <andrereigoto@gmai.com>
2018-09-04 15:59:42 +0100

    tth_new

---
+++ HELHC/tth_new/TreeProducer.py
@@ -0,0 +1,5 @@
+from heppy.framework.analyzer import Analyzer
+from heppy.statistics.tree import Tree
+from heppy.analyzers.ntuple import *
+from numpy import sign
+from ROOT import TFile, TLorentzVector
