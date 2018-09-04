cfac1cb71e75a7351eb5e8900ad3bc139f8aa79c
aReigoto <andrereigoto@gmai.com>
2018-09-04 17:32:47 +0100

    a

--- HELHC/tth_new/TreeProducer.py
+++ HELHC/tth_new/TreeProducer.py
@@ -1,5 +1,69 @@
+
 from heppy.framework.analyzer import Analyzer
 from heppy.statistics.tree import Tree
-from heppy.analyzers.ntuple import *
-from numpy import sign
-from ROOT import TFile, TLorentzVector
+from ROOT import TFile
+
+
+class SimpleTreeProducer(Analyzer):
+    '''Test analyzer creating a simple root tree.
+
+    Example::
+
+        tree = cfg.Analyzer(
+          SimpleTreeProducer,
+          tree_name = 'events',
+          tree_title = 'A simple test tree'
+        )
+
+    The TTree is written to the file C{simple_tree.root} in the analyzer directory.
+
+    @param tree_name: Name of the tree (Key in the output root file).
+    @param tree_title: Title of the tree.
+
+
+
+
+    from heppy.test.simple_example_cfg import *
+
+    from ROOT import TFile
+
+    files=["/eos/experiment/fcc/helhc/generation/DelphesEvents/helhc_v01/mgp8_pp_tth0123j_5f_hbb/events_000838653.root"]
+
+    f = TFile.Open(files[0], "read")
+
+    t = f.Get("events")
+
+    for i in t.GetListOfLeaves():
+        print i.GetName()
+
+
+    '''
+
+    def beginLoop(self, setup):
+        super(SimpleTreeProducer, self).beginLoop(setup)
+        self.rootfile = TFile('/'.join([self.dirName,
+                                        'simple_tree.root']),
+                              'recreate')
+        self.tree = Tree(self.cfg_ana.tree_name,
+                         self.cfg_ana.tree_title)
+        self.tree.var('test_variable')
+        self.tree.var('test_variable_random')
+
+    def process(self, event):
+        '''Process the event.
+
+        The input data must contain a variable called "var1",
+        which is the case of the L{test tree<heppy.utils.testtree>}.
+
+        The event must contain:
+         - var_random, which is the case if the L{RandomAnalyzer<heppy.analyzers.examples.simple.RandomAnalyzer.RandomAnalyzer>}
+         has processed the event.
+
+        '''
+        self.tree.fill('test_variable', event.input.var1)
+        self.tree.fill('test_variable_random', event.var_random)
+        self.tree.tree.Fill()
+
+    def write(self, setup):
+        self.rootfile.Write()
+        self.rootfile.Close()
---
+++ HELHC/tth_new/analysis.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import copy
+import math
+import heppy.framework.config as cfg
+import logging
+import imp
+# next 2 lines necessary to deal with reimports from ipython
+logging.shutdown()
+reload(logging)
+logging.basicConfig(level=logging.WARNING)
+
+sample = imp.load_source('heppylist', '/afs/cern.ch/work/h/helsens/public/FCCDicts/HELHC_heppySampleList_helhc_v01.py')
+
+comp = cfg.Component(
+    'example',
+    files=["/eos/experiment/fcc/helhc/generation/DelphesEvents/helhc_v01/mgp8_pp_tth0123j_5f_hbb/events_000838653.root"]
+)
